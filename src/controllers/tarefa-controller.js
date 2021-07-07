7c4995ff67522492cfac71d67a8b2db2a8d50a07
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-07 19:48:59 -0300

    nodemon e rotas post

--- src/controllers/tarefa-controller.js
+++ src/controllers/tarefa-controller.js
@@ -2,4 +2,8 @@ module.exports = app => {
     app.get('/tasks', (req, res) => {
         res.send('Rota ativada com GET e recurso Tarefas: Valores de tarefas devem ser retornados.')
     })
+
+    app.post('/tasks', (req, res) => {
+        res.send('Rota POST de tarefa ativada: tarefa adicionada ao banco de dados.')
+    })
 }
--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -2,4 +2,8 @@ module.exports = app => {
     app.get('/users', (req, res) => {
         res.send('Rota ativada com GET e recurso Usuários: Valores de usuários devem ser retornados.')
     })
+
+    app.post('/users', (req, res) => {
+        res.send('Rota POST de usuário ativada: usuário adicionado ao banco de dados.')
+    })
 }
