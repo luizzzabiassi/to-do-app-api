06c9b103ba8d5faacca69d77c57c961aaf3dab09
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-21 23:25:31 -0300

    implementando novas rotas no dao

--- src/dao/TaskDao.js
+++ src/dao/TaskDao.js
@@ -14,6 +14,42 @@ class TaskDao{
             })
         })
     }
+    getTitleTask(titulo){
+        return new Promise((resolve, reject) => {
+            this.db.all(`select * from TAREFAS where titulo = ?`, titulo, (err, rows) => {
+                if(err){
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    }
+    insertTask(task) {
+        return new Promise((resolve, reject) => {
+            this.db.run(`insert into TAREFAS(titulo, descricao, status, dataCriacao, id_usuario) values(?, ?, ?, ?, ?)`, Object.values(task), err => {
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(true);
+                }
+            })
+        })
+    }
+    deleteTask(titulo){
+        return new Promise((resolve, reject) => {
+            this.db.run(`delete from TAREFAS where titulo = ?`, titulo, err => {
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(true);
+                }
+            })
+        })
+    }
 }
 
 module.exports = TaskDao
--- src/dao/UserDao.js
+++ src/dao/UserDao.js
@@ -4,12 +4,48 @@ class UserDao{
     }
     getAllUsers(){
         return new Promise((resolve, reject) => {
-            this.db.all("select * from USUARIOS", (err, rows) => {
+            this.db.all(`select * from USUARIOS`, (err, rows) => {
                 if(err){
                     reject(err)
                 }
                 else{
                     resolve(rows)
+                }
+            })
+        })
+    }
+    getEmailUser(email){
+        return new Promise((resolve, reject) => {
+            this.db.all(`select * from USUARIOS where email = ?`, email, (err, rows) => {
+                if(err){
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+        })
+    }
+    insertUser(user){
+        return new Promise((resolve, reject) => {
+            this.db.run(`insert into USUARIOS(nome, email, senha) values(?, ?, ?)`, Object.values(user), err => {
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(true);
+                }
+            })
+        })
+    }
+    deleteUser(email){
+        return new Promise((resolve, reject) => {
+            this.db.run(`delete from USUARIOS where email = ?`, email, err => {
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(true);
                 }
             })
         })
