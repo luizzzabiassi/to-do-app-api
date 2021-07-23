330a5d988d16dc2acc61b0192ec256c2e66a7bd3
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-23 18:34:48 -0300

    resolvendo bug jest

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
 
 const rotaUsers = require('./controllers/usuario-controller')
 const rotaTasks = require('./controllers/tarefa-controller')
 
 const User = require('./models/usuario-model')
 const Task = require('./models/tarefa-model')
-
 const db = require('./infra/sqlite-db')
 
 app.use(express.json())
@@ -18,6 +16,4 @@ app.use(cors())
 rotaUsers(app, db)
 rotaTasks(app, db)
 
-app.listen(port, () => {
-    console.log(`Servidor rodando: http://localhost:${port}`);
-})
+module.exports = app
