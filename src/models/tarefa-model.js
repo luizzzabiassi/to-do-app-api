4242d65e4674253c73b18a0b4203c7fddde494f5
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-26 21:19:16 -0300

    install npm moment

--- src/models/tarefa-model.js
+++ src/models/tarefa-model.js
@@ -1,9 +1,11 @@
+const moment = require('moment')
+
 class Task {
-    constructor(titulo, descricao, status, data_criacao, userId){
+    constructor(titulo, descricao, status, userId){
         this.titulo = titulo;
         this.descricao = descricao;
         this.status = status;
-        this.data_criacao = data_criacao;
+        this.data_criacao = moment(Date.now()).format('YYYY-MM-DD');
         this.userId = userId
     }
 }
