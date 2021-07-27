04811efab8db71d8ec880ebf91a5b3c169d9e212
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-26 21:25:46 -0300

    port env para heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const port = 3000
 
-app.listen(port, () => {
-    console.log(`Servidor rodando: http://localhost:${port}`);
+app.listen(process.env.PORT || port, () => {
+    console.log(`Servidor rodando: http://localhost:${process.env.PORT || port}`);
 })
